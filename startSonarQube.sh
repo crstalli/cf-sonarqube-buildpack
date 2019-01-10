1db2a7f8fa5f39accbe23f8009e0547d0a7e0732
Christopher Stallings <crstalli@syr.edu>
2019-01-10 13:27:50 -0500

    Update startSonarQube.sh

--- startSonarQube.sh
+++ startSonarQube.sh
@@ -6,8 +6,8 @@ export PATH=$PATH:/home/vcap/app/.java/bin
 
 # Replace all environment variables with syntax ${MY_ENV_VAR} with the value
 # thanks to https://stackoverflow.com/questions/5274343/replacing-environment-variables-in-a-properties-file
-perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg; s/\$\{([^}]+)\}//eg' ./sonar.properties > ./sonar_replaced.properties
-mv ./sonar_replaced.properties ./sonar.properties
+#perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg; s/\$\{([^}]+)\}//eg' ./sonar.properties > ./sonar_replaced.properties
+#mv ./sonar_replaced.properties ./sonar.properties
 
 cat ./sonar.properties
 
